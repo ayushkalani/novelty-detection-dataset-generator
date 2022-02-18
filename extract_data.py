33ed0153b181e2d32999f24e753702247dd7c1c5
mayank18garg <mayank.ind18@gmail.com>
2022-02-18 13:13:45 -0700

    Novelty Data Utils

--- extract_data.py
+++ extract_data.py
@@ -4,6 +4,7 @@ import logging
 import re
 import pandas as pd
 import copy
+from Novelty_utils import novelty_mapping
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(message)s",
@@ -14,19 +15,15 @@ logging.basicConfig(
 )
 PATH_TO_JSON = 'json/'
 PLAYER_TO_IGNORE = "player_1"
-PLAYER_KEYS_TO_IGNORE = ["full_color_sets_possessed", "assets", "mortgaged_assets"]
+PLAYER_KEYS_TO_IGNORE = ["full_color_sets_possessed", "assets"]
 NUMBER_OF_PLAYERS = 4
-# WHITELISTED_ACTIONS = ["free_mortgage", "make_sell_property_offer", "sell_property", "sell_house_hotel", "accept_sell_property_offer",
-#                        "skip_turn", "concluded_actions", "mortgage_property", "improve_property", "use_get_out_of_jail_card",
-#                        "pay_jail_fine", "roll_die", "buy_property", "make_trade_offer",
-#                        "accept_trade_offer", "pre_roll_arbitrary_action", "out_of_turn_arbitrary_action",
-#                        "post_roll_arbitrary_action", "accept_arbitrary_interaction"]
 
 WHITELISTED_ACTIONS = {"free_mortgage":0, "make_sell_property_offer":1, "sell_property":2, "sell_house_hotel":3, "accept_sell_property_offer":4,
                        "skip_turn":5, "concluded_actions":6, "mortgage_property":7, "improve_property":8, "use_get_out_of_jail_card":9,
                        "pay_jail_fine":10, "roll_die":11, "buy_property":12, "make_trade_offer":13,
                        "accept_trade_offer":14, "pre_roll_arbitrary_action":15, "out_of_turn_arbitrary_action":16,
                        "post_roll_arbitrary_action":17, "accept_arbitrary_interaction":18}
+
 
 # number of locations on board - 36
 LOCATIONS = {'Illinois Avenue', 'Go to Jail', 'Luxury Tax', 'Go', 'B&O Railroad', 'Atlantic Avenue', 'Boardwalk', 'Marvin Gardens', 'Pacific Avenue', 'Water Works', 'Mediterranean Avenue', 'Community Chest', 'Tennessee Avenue', 'Baltic Avenue', 'North Carolina Avenue', 'Income Tax', 'Reading Railroad', 'Oriental Avenue', 'Chance', 'Indiana Avenue', 'Short Line', 'Vermont Avenue', 'Ventnor Avenue', 'Connecticut Avenue', 'In Jail/Just Visiting', 'St. Charles Place', 'Park Place', 'Electric Company', 'States Avenue', 'Virginia Avenue', 'Pennsylvania Railroad', 'St. James Place', 'Pennsylvania Avenue', 'New York Avenue', 'Free Parking', 'Kentucky Avenue'}
@@ -52,8 +49,11 @@ def natural_sort(l):
         
 def process_history(data, current_timestep):
     for step in data["history"]:
+        function_name = step["function"]
+        if function_name in novelty_mapping.keys():
+            function_name = novelty_mapping[function_name] 
         try:
-            if step["time_step"] == current_timestep and step["function"] in WHITELISTED_ACTIONS.keys() and step["param"]:
+            if step["time_step"] == current_timestep and function_name in WHITELISTED_ACTIONS.keys() and step["param"]:
                 # check player first, and then self, because self can be a location too
                 player_name = ""
                 if  step["param"].get("player", None):
@@ -68,7 +68,7 @@ def process_history(data, current_timestep):
             pass
         except ValueError:
             logging.error("ValueError while process_history %s", current_timestep)
-            pass
+            passgit 
 
 def encode_player_assets(data):
     location = copy.deepcopy(LOCATION_VECTOR)
@@ -111,15 +111,15 @@ def read_game_json():
         # clean
         for i in range(1, NUMBER_OF_PLAYERS + 1):
             for key_to_delete in PLAYER_KEYS_TO_IGNORE:
-                data["players"]["player_"+str(i)].pop(key_to_delete, None)
-        data.pop("location_sequence", None)
-        data.pop("cards", None)
-        data.pop("die_sequence", None)
-        data.pop("history", None)
-        data.pop("locations", None)
+                if data["players"]["player_"+str(i)].get(key_to_delete, None):
+                    del data["players"]["player_"+str(i)][key_to_delete]
+        del data["location_sequence"]
+        del data["locations"]
+        del data["cards"]
+        del data["die_sequence"]
+        del data["history"]
         df_list.append(data)
         of.close()
-        break
     write_csv(df_list)
 
 def write_csv(write):
@@ -127,7 +127,7 @@ def write_csv(write):
     for col in PLAYER_BOOLEAN_COLUMNS:
         if col in df.columns:
             df[col] = df[col].astype(int)
-    df.to_csv('test_2.csv', index=False, encoding='utf-8')
+    df.to_csv('test_1.csv', index=False, encoding='utf-8')
 
 if __name__ == "__main__":
     try:
