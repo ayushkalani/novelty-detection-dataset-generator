edf3486156d6e771d442f0f0b6292b75656db246
Ayush Kalani <kalani.ayush@gmail.com>
2022-02-18 14:18:49 -0700

    Revert "Novelty Data Utils"
    This reverts commit 33ed0153b181e2d32999f24e753702247dd7c1c5.

--- Novelty_utils.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-
-novelty_mapping = {"buy_discount_property":"buy_property", "buy_property_right":"buy_property", "buy_property_rightplayers":"buy_property", "improvePropertyRed_novelty":"improve_property"}
